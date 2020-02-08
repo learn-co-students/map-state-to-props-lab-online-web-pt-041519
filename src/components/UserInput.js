6e8c5841a566f5aec816f89d79f7a55eb668c1e1
earthctzn <earthctzn1@gmail.com>
2020-02-08 16:32:01 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -17,8 +17,9 @@ class UserInput extends Component {
   handleOnSubmit = (event) => {
     event.preventDefault();
     this.props.dispatch({type: 'ADD_USER', user: this.state})
-  }
 
+  }
+  
   render() {
     return(
       <form onSubmit={this.handleOnSubmit}>
